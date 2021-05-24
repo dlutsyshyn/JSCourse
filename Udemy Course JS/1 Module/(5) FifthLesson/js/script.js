b354ea28ff4ea5fbe370264b6d15b91b79cd2ffa
dlutsyshyn <dlutsyshyn@student.ucode.world>
2021-05-24 20:49:08 +0300

    24.05

---
+++ Udemy Course JS/1 Module/(5) FifthLesson/js/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+/* let num = 20;
+
+function showFirstMassage (text) {
+    console.log(text);
+    let num = 15;
+    console.log(num);
+}
+
+showFirstMassage ('lol');
+console.log(num); */
+
+/* function calc (a, b) {
+    return (a+b);    
+}
+
+console.log(calc(4, 3));
+console.log(calc(7, 8));
+console.log(calc(10, 40)); */
+
+/* function ret() {
+    let num = 50;
+    return num;
+}
+*/
+
+const calc = (a, b) => a + b;
+
+console.log(calc());
